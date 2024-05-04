e5985bd10b199b50de67c7b76bb1685eec065ec7
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-05-03 22:04:47 -0600

    Modified properties app

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -1,22 +1,37 @@
 from django.db import models
-from abstracts import AbstractModel
+
+from apps.abstracts import AbstractModel
 
 # Create your models here.
+
+PROPERTY_TYPE_CHOICES = {
+    "AP": "Apartment",
+    "LA": "Land",
+    "HO": "House",
+}
+
 
 class Property(AbstractModel):
     title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.PositiveIntegerField()
-    num_bedrooms = models.PositiveIntegerField() 
+    num_bedrooms = models.PositiveIntegerField()
     num_bathrooms = models.PositiveIntegerField()
-    property_type = models.CharField(max_length=15)
+    property_type = models.CharField(
+        max_length=2,
+        choices=PROPERTY_TYPE_CHOICES,
+        blank=False,
+    )
+
 
 class Apartment(Property):
     num_floors = models.PositiveIntegerField()
 
+
 class Land(Property):
     area = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class House(Property):
     num_floors = models.PositiveIntegerField()
