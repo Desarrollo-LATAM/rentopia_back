c92deb0b79e7465b66c3178cec47b1f56cb43e0a
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-05-03 23:52:34 -0600

    Corrected path of imports in properties app

--- apps/properties/api/serializers.py
+++ apps/properties/api/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
-from properties.models import Property, Apartment, Land, House
+
+from apps.properties.models import Apartment, House, Land, Property
+
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ApartmentSerializer(PropertySerializer):
     class Meta(PropertySerializer.Meta):
         model = Apartment
-        fields = PropertySerializer.Meta.fields + ['num_floors']
+        fields = PropertySerializer.Meta.fields + ["num_floors"]
+
 
 class LandSerializer(PropertySerializer):
     class Meta(PropertySerializer.Meta):
         model = Land
-        fields = PropertySerializer.Meta.fields + ['area']
+        fields = PropertySerializer.Meta.fields + ["area"]
+
 
 class HouseSerializer(PropertySerializer):
     class Meta(PropertySerializer.Meta):
         model = House
-        fields = PropertySerializer.Meta.fields + ['num_floors']
+        fields = PropertySerializer.Meta.fields + ["num_floors"]
