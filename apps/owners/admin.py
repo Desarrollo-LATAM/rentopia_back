ebfe544f66a1a2e1528de93e71372af99cb7433b
Miguel Angel Lopez <miguel.lopezm.dev@gmail.com>
2024-05-03 20:42:55 -0600

    Added fields for admin panel in apps users, owners and tenants

--- apps/owners/admin.py
+++ apps/owners/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Owner
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "user_id",
+        "name",
+        "last_name",
+        "address",
+        "photo",
+        "rating_user",
+        "phone",
+    )
+    search_fields = ("name", "last_name", "phone")
+    ordering = ("user_id",)
+
+
+admin.site.register(Owner, OwnerAdmin)
--- apps/tenants/admin.py
+++ apps/tenants/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tenant
+
+
+class TenantAdmin(admin.ModelAdmin):
+    list_display = (
+        "user_id",
+        "name",
+        "last_name",
+        "rating_user",
+        "phone",
+    )
+    search_fields = ("name", "last_name", "phone")
+    ordering = ("user_id",)
+
+
+admin.site.register(Tenant, TenantAdmin)
