978a55ee5c9519ab645ddff3e70aca82300bc883
Sil_Palaoro <silvinapalaoro@gmail.com>
2024-06-17 13:38:31 -0300

    PATCH with time limit and user must be the sender

--- apps/messaging/api/viewsets.py
+++ apps/messaging/api/viewsets.py
@@ -57,6 +57,30 @@ class MessageViewSet(AbstractViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        # Verificar si el usuario es el remitente del mensaje
+        if instance.sender != request.user:
+            return Response({'detail': 'No tienes permiso para editar este mensaje.'}, status=status.HTTP_403_FORBIDDEN)
+
+        # Verificar si el mensaje fue enviado hace más de una semana
+        one_week_ago = timezone.now() - timezone.timedelta(days=7)
+        if instance.created < one_week_ago:
+            return Response({'detail': 'No puedes editar un mensaje enviado hace más de una semana.'}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+    
+    
     @action(detail=True, methods=['delete'])
     def delete(self, request, pk=None):
         message = self.get_object()
